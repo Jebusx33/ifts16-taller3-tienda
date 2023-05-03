396105dc81d391e19eedcf84034d0875bd5c0879
Jesus <gabrielj82@gmail.com>
2023-05-03 20:30:57 -0300

    models, routes, controller de  admin y cliente

--- Back/routes/cliente.js
+++ Back/routes/cliente.js
@@ -1,7 +1,7 @@
 'use strict'
 var express = require('express');
 //const { registro_cliente } = require('../controllers/clienteController');
-var clienteController = requiere('../controller/ClienteController')
+var clienteController = require('../controllers/ClienteController')
 var api = express.Router();
-api.post('/registro_cliente', clienteController,registro_cliente);
+api.post('/registro_cliente', clienteController.registro_cliente);
 module.exports = api;
