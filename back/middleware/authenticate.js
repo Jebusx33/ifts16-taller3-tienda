19e82320e54db65541bac5426b9c4c1c209ce7c7
CinthiaSabino <Cinthia.Sabino@comafi.com.ar>
2023-08-27 12:08:24 -0300

    Se arregla conexion con el BE

--- back/middleware/authenticate.js
+++ back/middleware/authenticate.js
@@ -12,27 +12,29 @@ exports.auth = function (req, res, next) {
   }
 
   //segmenta el token en 3 partes
-  var token = req.headers.authorization.replace(/['"]+/g, '');
+  var token = req.headers.authorization.replace(/[""]+/g, '');
   var segment = token.split('.');
   //console.log(token);
-  //console.log(segment);
+ // console.log(segment);
   //valida el token si la longitud es distinta a 3
   if (segment.length != 3) {
-    return res.status(403).send({ message: 'InvalidToken' });
+    return res.status(403).send({ message: 'InvalidTokenDiff3' });
   } else {
+    if (segment && token.startsWith('Bearer ')) {
+      // Extrae el token eliminando "Bearer " del encabezado
+      token = token.substring(7);
     try { //decodifica el token   
       var payload = jwt.decode(token, secret);
-
+      console.log(payload);
       if (payload.exp <= moment.unix()) {//si la fecha expiro
-        return res.status(403).send({ message: 'TokenWxpirado' });
-      } else {
-
+        return res.status(403).send({ message: 'TokenExpirado' });
       }
 
     } catch (error) {
+      //console.log(error);
       return res.status(403).send({ message: "InvalidToken" });
     }
-
+    }
   }
   req.user = payload;
 
