64ec0227a1bbd15ed1644d0d3b4194d23eba93ab
CinthiaSabino <Cinthia.Sabino@comafi.com.ar>
2023-08-27 18:50:56 -0300

    Add Config Front and Apis

--- back/controllers/ConfigController.js
+++ back/controllers/ConfigController.js
@@ -1,18 +1,14 @@
 'use strict'
-var config = require('../models/config');
+const Config = require('../models/config');
+const fs = require('fs');
+const path = require('path');
 
-const actualiza_config_admin = async function(req,res){
+
+const obtener_config_admin = async function(req,res){
     if(req.user){
         if(req.user.role == 'admin'){
-            
-            await config.create({
-                categorias: [],
-                titulo: 'Createx',
-                logo: 'logo.png',
-                serie: '0001',
-                correlativo: '0000001',
-            });
-
+            let reg =await Config.findById({_id:'64ea50d5a61984d7fb807294'});
+            res.status(200).send({data:reg});
         }else{
             res.status(500).send({messege: 'NoAccess'});
         }
@@ -22,6 +18,66 @@ const actualiza_config_admin = async function(req,res){
 }
 
 
+const actualiza_config_admin = async function(req,res){
+    if(req.user){
+        if(req.user.role == 'admin'){
+            let data = req.body;
+           //console.log(data)
+            if(req.files){
+                //FALTA DESARROLLAR
+            }else{
+                let reg = await Config.findByIdAndUpdate({_id:'64ea50d5a61984d7fb807294'},{
+                    categorias: data.categorias,
+                    titulo: data.titulo,
+                    serie: data.serie,
+                    correlativo: data.correlativo
+                });
+
+                res.status(200).send({data:reg})
+            }
+            
+
+            /*  Se creo el primer valor en la DB 
+            await Config.create({
+                categorias: [],
+                titulo: 'Pepe',
+                logo: 'logo.png',
+                serie:1,
+                correlativo: 1
+            });*/
+
+        }else{
+            res.status(500).send({messege: 'NoAccess'});
+        }
+    }else{
+        res.status(500).send({messege: 'NoAccess'});
+    }
+}
+
+const obtener_logo = async function(req,res){
+    var img = req.params['img'];
+    console.log(img)
+
+    fs.stat('./uploads/configuraciones/' + img, function(err){
+        if(!err){
+            let path_img = './uploads/configuraciones/' + img;
+            res.status(200).sendFile(path.resolve(path_img));
+        }else{
+            let path_img = './uploads/default.jpg/';
+            res.status(200).sendFile(path.resolve(path_img));
+        }
+    });
+
+}
+
+/*const obtener_config_publico = async function(req,res){
+    let reg =await Config.findById({_id:'64ea50d5a61984d7fb807294'});
+    res.status(200).send({data:reg});
+}*/
+
 module.exports = {
-    actualiza_config_admin
+    actualiza_config_admin,
+    obtener_config_admin,
+    obtener_logo,
+    //obtener_config_publico
 }
--- back/routes/config.js
+++ back/routes/config.js
@@ -4,7 +4,13 @@ var configController = require('../controllers/ConfigController')
 
 var api = express.Router();
 var auth = require('../middleware/authenticate');
+var multiparty = require('connect-multiparty');
+var path = multiparty({uploadDir: './uploads/configuraciones'})
 
-api.get('/actualiza_config_admin/', auth.auth, configController.actualiza_config_admin);
+//api.post('/actualiza_config_admin/', auth.auth, configController.actualiza_config_admin); //Creo primer config
+api.put('/actualiza_config_admin/:id', [auth.auth,path], configController.actualiza_config_admin);
+api.get('/obtener_config_admin', auth.auth, configController.obtener_config_admin);
+api.get('/obtener_logo/:img', configController.obtener_logo);
+//api.get('/obtener_config_publico', configController.obtener_config_publico);
 
 module.exports = api;
