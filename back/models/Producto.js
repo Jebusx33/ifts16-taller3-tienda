b353fa47ac230a1adadde37968dc12fa51c04b09
Jesús Arias <40777283+Jebusx33@users.noreply.github.com>
2023-09-02 07:04:25 -0300

    Update Producto.js

--- back/models/Producto.js
+++ back/models/Producto.js
@@ -1,29 +1,24 @@
 'use strict'
 
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
 var Schema = mongoose.Schema;
 
 var ProductoSchema = Schema({
-    titulo: {type: String, required: true},
-    slug: {type: String, required: true},
-    portada: {type: String, required: true},
-    precio_antes_soles: {type: Number,default: 0, required: true},
-    precio_antes_dolares: {type: Number,default: 0, required: true},
-    precio: {type: Number, required: true},
-    precio_dolar: {type: Number, required: true},
-    sku: {type: String, required: true},
-    descripcion: {type: String, required: true},
-    contenido: {type: String, required: true},
-    nventas: {type: Number, default: 0, required: true},
-    categoria: {type: String, required: true},
-    visibilidad: {type: String, required: true},
+    titulo: { type: String, required: true },
+    slug: { type: String, required: true },
+    galeria: [{ type: Object, required: false }],
+    portada: { type: String, required: true },
+    precio: { type: String, required: true },
+    descripcion: { type: String, required: true },
+    contenido: { type: String, required: true },
+    stock: { type: Number, default: 0, required: false },
+    nventas: { type: Number, default: 0, required: true },
+    npuntos: { type: Number, default: 0, required: true },
+    categoria: { type: String, required: true },
 
-    stock: {type: Number,default:0 ,required: false},
-    galeria: [{type: Object, required: false}],
-    peso: {type: String, required: false},
-
-    estado: {type: String, default: 'Edicion', required: true},
-    createdAt: {type:Date, default: Date.now, require: true}
+    estado: { type: Date, default: 'Edicion', required: true },
+    //fecha y hora de creacion en formato date(pasarlo a timestamp)
+    createdAt: { type: Date, default: Date.now, required: true },
 });
 
-module.exports =  mongoose.model('producto',ProductoSchema);
+module.exports = mongoose.model('producto', ProductoSchema);
