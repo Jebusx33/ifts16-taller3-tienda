23bbceaf3d47abc660ddbeda90d1f2226fbef118
Cristian <cristianjelovcic@gmail.com>
2023-11-14 01:11:17 -0300

    routing direcciones

--- tienda/src/app/app.routing.ts
+++ tienda/src/app/app.routing.ts
@@ -9,7 +9,6 @@ import { AuthGuard } from "./guards/auth.guard";
 import { IndexProductoComponent } from "./components/productos/index-producto/index-producto.component";
 import { ShowProductoComponent } from "./components/productos/show-producto/show-producto.component";
 import { CarritoComponent } from "./components/carrito/carrito.component";
-/*import { CarritoComponent } from "./components/carrito/carrito.component";
 import { DireccionesComponent } from "./components/usuario/direcciones/direcciones.component";
 /*import { ContactoComponent } from "./components/contacto/contacto.component";
 import { IndexOrdenesComponent } from "./components/usuario/ordenes/index-ordenes/index-ordenes.component";
@@ -27,9 +26,8 @@ const appRoute : Routes =[
     {path: 'productos/categoria/:categoria', component: IndexProductoComponent},
     {path: 'productos/:slug', component: ShowProductoComponent},
     {path: 'carrito', component: CarritoComponent, canActivate: [AuthGuard]},
-    //{path: 'carrito', component: CarritoComponent, canActivate: [AuthGuard]},
-    /*{path: 'cuenta/direcciones', component: DireccionesComponent, canActivate: [AuthGuard]},
-    {path: 'cuenta/ordenes', component: IndexOrdenesComponent, canActivate: [AuthGuard]},
+    {path: 'cuenta/direcciones', component: DireccionesComponent, canActivate: [AuthGuard]},
+    /*{path: 'cuenta/ordenes', component: IndexOrdenesComponent, canActivate: [AuthGuard]},
     {path: 'cuenta/direcciones', component: DireccionesComponent, canActivate: [AuthGuard]},
    /* {path: 'cuenta/ordenes', component: IndexOrdenesComponent, canActivate: [AuthGuard]},
     {path: 'cuenta/ordenes/:id', component: DetalleOrdenComponent, canActivate: [AuthGuard]},
