6cb37e6cd2110286dde7e2c9d56a8d1559ba43b7
CinthiaSabino <Cinthia.Sabino@comafi.com.ar>
2023-11-23 21:51:39 -0300

    fix descuento

--- back/routes/descuento.js
+++ back/routes/descuento.js
@@ -4,7 +4,7 @@ var express = require('express');
 var descuentoController = require('../controllers/descuentoController');
 
 var api = express.Router();
-var auth = require('../middlewares/authenticate');
+var auth = require('../middleware/authenticate');
 var multiparty = require('connect-multiparty');
 var path = multiparty({uploadDir: './uploads/descuentos'});
 
